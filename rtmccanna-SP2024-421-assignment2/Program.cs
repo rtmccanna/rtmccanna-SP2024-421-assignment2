5bb8014427f88db83b184c162004dd225a5eeda6
robmc <robmc@RTM_LAPTOP24>
2024-09-26 22:02:13 -0500

    Add project files.

---
+++ rtmccanna-SP2024-421-assignment2/Program.cs
@@ -0,0 +1,16 @@
+namespace rtmccanna_SP2024_421_assignment2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
---
+++ rtmccanna-SP2024-421-assignment2/wwwroot/script.js
@@ -0,0 +1,52 @@
+﻿var len;
+var results = '';
+
+function apiSearch() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "e57fb535309543a5a54b2002bf366c09");
+        },
+        type: "GET",
+    })
+        .done(function (data) {
+            len = data.webPages.value.length;
+            for (i = 0; i < len; i++) {
+                results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+            }
+
+            $('#searchResults').html(results);
+            $('#searchResults').dialog();
+        })
+        .fail(function () {
+            alert("error");
+        });
+}
+
+function changeBackgroundImage() {
+    document.body.style.backgroundImage = "url'(https://plus.unsplash.com/premium_photo-1727186144804-d38ddc960b3f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)'"
+}
+
+function displayTime() {
+    $("#time").dialog();
+}
+
+function showCurrentTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const formattedTime = `${hours}:${minutes}`;
+    $('#time').text(formattedTime);
+    $('time').dialog();
+}
+
+$(document).ready(function() {
+    $('#showTimeButton').on('click', showCurrentTime);
+});
